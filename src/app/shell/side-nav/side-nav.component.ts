1efc018aca707935e41a14c0d9263ebe26d0cf05
BFCLimited <4545pavan@gmail.com>
2022-02-15 19:17:02 +0530

    SideNav Static Content added

--- src/app/shell/side-nav/side-nav.component.ts
+++ src/app/shell/side-nav/side-nav.component.ts
@@ -12,24 +12,112 @@ export class SideNavComponent implements OnInit {
 
   newSideNav = [
     {
-      id: 'dashboardmenu',
-      name: 'Dashboard',
+      id: 'Professional',
+      name: 'Professional',
       display: true,
-      parentRouteName: 'dashboard',
-      routeName: 'main-view',
-      icon: 'home',
+      parentRouteName: 'home',
+      routeName: 'ApplianceRepair',
+      icon: 'work',
       child: [],
     },
 
     {
-      id: 'cashierdashboardmenu',
-      name: 'Cashier Dashboard',
+      id: 'FeedBack_Complaints',
+      name: 'Feed Back / Complaints',
       display: true,
-      parentRouteName: 'cashier-dashboard',
-      routeName: 'main-view',
-      icon: 'account_balance_wallet',
+      parentRouteName: 'home',
+      routeName: 'ApplianceRepair',
+      icon: 'feedback',
       child: [],
     },
+
+    {
+      id: 'Services',
+      name: 'Services',
+      display: true,
+      parentRouteName: 'not-using',
+      routeName: null,
+      icon: 'app_registration',
+      child: [
+        {
+          id: 'ApplianceRepair',
+          name: 'Appliance Repair',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'local_laundry_service',
+        },
+
+        {
+          id: 'Construction',
+          name: 'Construction',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'location_city',
+        },
+
+        {
+          id: 'EventManagment',
+          name: 'Event Managment',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'whatshot',
+        },
+
+        {
+          id: 'Cleaning_Pest Control',
+          name: 'Cleaning / Pest Control',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'bug_report',
+        },
+
+        {
+          id: 'Transport_Logistics',
+          name: 'Transport / Logistics',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'local_shipping',
+        },
+        {
+          id: 'Vehicle_Repair_Service',
+          name: 'Vehicle Repair/Service',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'warning',
+        },
+        {
+          id: 'Farming_Service',
+          name: 'Farming Service',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'local_florist',
+        },
+        {
+          id: 'Network_Services',
+          name: 'Network Services',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'settings_ethernet',
+        },
+        {
+          id: 'gadgets_Service',
+          name: 'gadgets Service',
+          display: true,
+          parentRouteName: 'home',
+          routeName: 'ApplianceRepair',
+          icon: 'phonelink',
+        },
+      ],
+    },
+
   ]
 
   ngOnInit(): void {
