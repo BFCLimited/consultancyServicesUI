0fb8a5d766f8bbce2250640cd26853ad54bd2bb7
BFCLimited <4545pavan@gmail.com>
2022-02-19 04:12:49 +0530

    Added Professional Registration form as StepbyStep

--- src/app/@Modules/professionals-cat/professionals-cat-routing.module.ts
+++ src/app/@Modules/professionals-cat/professionals-cat-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppliancesCatComponent } from '../appliances-cat/appliances-cat/appliances-cat.component';
 import { ProfessionalsHighlightsComponent } from './professionals-highlights/professionals-highlights.component';
+import { ProfessionalsRegistrationComponent } from './professionals-registration/professionals-registration.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
@@ -11,7 +12,7 @@ const routes: Routes = [
     component : ProfessionalsHighlightsComponent,
     children:[
       {
-        path: "main-view", component : ProfessionalsHighlightsComponent
+        path: "Registration", component : ProfessionalsRegistrationComponent
       },
     ],
   },
--- src/app/@Modules/professionals-cat/professionals-highlights/professionals-highlights.component.ts
+++ src/app/@Modules/professionals-cat/professionals-highlights/professionals-highlights.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProfessionalsRegistrationComponent } from '../professionals-registration/professionals-registration.component';
 
 @Component({
   selector: 'app-professionals-highlights',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessionalsHighlightsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _openDialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  OpenProfessionalRegistration(){
+    const dialogRefProfessionalRegistrationPage = this._openDialog.open(
+      ProfessionalsRegistrationComponent,
+      {
+        panelClass: "full-screen-dialog",
+      }
+    );
+    dialogRefProfessionalRegistrationPage
+      .beforeClosed()
+      .subscribe((data) => {
+        if (data) {
+        }
+      });
   }
 
 }
